a8864b75e50f08b4bd0c2ae977cac461c851a01c
Weslley David <weslleyvwp@hotmail.com>
2021-06-05 18:31:57 -0300

    terceiro commit

--- folha_2/q9_numero_primo.js
+++ folha_2/q9_numero_primo.js
@@ -1,4 +1,3 @@
-//AINDA NÃO TERMINEI
 //9. Leia 1 (um) número entre 0 e 100, verifique e escreva se o número é ou não primo.
 const input = require('prompt-sync')()
 function main(){
@@ -6,10 +5,10 @@ function main(){
     const numero = input('Escreva um número (intervalo de 1 a 100): ')
     
     // Processamento e saída
-    if (numero[0] == numero[1]){
-        console.log('Desena igual a unidade')
+    if (numero == 2 || numero == 3 || numero == 5 || numero == 7 || numero == 11 || numero == 13 || numero == 17 || numero == 19 || numero == 23 || numero == 29 || numero == 31 || numero == 37 || numero == 41 || numero == 43 || numero == 47 || numero == 53 || numero == 59 || numero == 61 || numero == 67 || numero == 71 || numero == 73 || numero == 79 || numero == 83 || numero == 89 || numero == 97){
+        console.log(numero ,' é primo')
     }else{
-        console.log('Algarismos diferentes entre si')
+        console.log(numero,' não é primo')
     }
 }
 main()
