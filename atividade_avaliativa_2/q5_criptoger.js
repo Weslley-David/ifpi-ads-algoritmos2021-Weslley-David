74c51efde15c0590aaa661a24a330be9740e6512
Weslley David <weslleyvwp@hotmail.com>
2021-07-12 21:56:22 -0300

    questão incompleta

--- atividade_avaliativa_2/q5_criptoger.js
+++ atividade_avaliativa_2/q5_criptoger.js
@@ -1,21 +1,29 @@
-const prompt = require('prompt-sync')()
-
 /*5. (q5_criptoger.js) Criptografia de Roger: Receba uma frase(letra números e
 espaços apenas) e criptografia-a usando o algoritmo de roger:*/
+//apenas a primeira parte completa com alguns bug ;-;
+const input = require('prompt-sync')()
 function main(){
-    const palavra = 'teste'
+    frase_final = ''
+    const frase = input('digite uma frase: ')
+    frase_split = frase.split(' ')
+    for (let i = 0; i < frase_split.length; i++) {
+        frase_invertida = inverter_partes(frase_split[i])
+        frase_final = frase_final + ' ' + frase_invertida
+    }
+    console.log(frase_final)
+}
+function inverter_partes(frase){
+    const palavra = frase
     
     let frase_invertida = '' //invertendo as partes da frase
 
-        for (let i = Math.floor((palavra.length - 1)/2); i >= 0; i--) {
+        for (let i = (Math.floor(palavra.length/2)) - 1; i >= 0; i--) {
             frase_invertida = frase_invertida + palavra[i]
         }
-        console.log(frase_invertida)
-        
-        for (let i = Math.floor((palavra.length - 1)/2) + 1; i >= palavra.length; i++) {
+        for (let i = (Math.floor(palavra.length/2)); i < palavra.length; i++) {
             frase_invertida = frase_invertida + palavra[i]
-            
         }
-        console.log(frase_invertida)
+
+        return frase_invertida
 }
 main()
